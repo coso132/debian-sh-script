201e9b1eef024e0880dc0678f7d6ba72eb782464
coso132 <cosof323@gmail.com>
2024-04-26 17:35:15 +0200

    Primo test

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt install zsh curl git gh vim neofetch btop curl cmake gcc -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+Y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
