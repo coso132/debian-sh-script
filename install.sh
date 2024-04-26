d60439ba12baabdd0ba4dcf7383c78312852baeb
Franco <84221179+coso132@users.noreply.github.com>
2024-04-26 18:10:32 +0200

    bob nvim

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 sudo apt install zsh curl git gh vim neofetch btop curl cmake gcc -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-. "$HOME/.cargo/env"
+# exec '. "$HOME/.cargo/env"'
+cargo install bob-nvim
+bob use stable
+sudo ln -s /home/$USER/.local/share/bob/nvim-bin/nvim /usr/bin/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
