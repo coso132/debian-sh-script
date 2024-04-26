910b09e5600e75f1bebf765862c457a65909a40e
Franco <84221179+coso132@users.noreply.github.com>
2024-04-26 17:53:51 +0200

    removed xdotool

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-sudo apt install xdotool zsh curl git gh vim neofetch btop curl cmake gcc -y
+sudo apt install zsh curl git gh vim neofetch btop curl cmake gcc -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-xdotool key Y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-xdotool key enter
 . "$HOME/.cargo/env"
+
