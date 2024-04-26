5bb81ba13b598b1c3a16311c52ffad84dbc1dbe5
Franco <84221179+coso132@users.noreply.github.com>
2024-04-26 17:55:14 +0200

    bob

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@ sudo apt install zsh curl git gh vim neofetch btop curl cmake gcc -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 . "$HOME/.cargo/env"
-
