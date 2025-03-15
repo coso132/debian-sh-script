a2e2c289a91ae9fb0dcc5f9ae9fd341f9497afa8
Franco Antonini <84221179+coso132@users.noreply.github.com>
2025-03-15 16:39:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt install zsh curl git gh vim neofetch btop curl cmake gcc -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # exec '. "$HOME/.cargo/env"'
 cargo install bob-nvim
 bob use stable
@@ -8,3 +8,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # ADD NVIM CONFIGS
 # ADD XFCE CONFIGS
 # ADD FONTS
+# ADD PYTHON
+# FLATPAK
+# BRAVE 
+# VSCODE FOR NOTEBOOK
+# 
